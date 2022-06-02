7a0ff3b688a66c51a2798b683f0de66d13771b8a
shreyash547 <106372879+shreyash547@users.noreply.github.com>
2022-06-02 16:08:50 +0530

    Create JenkinsPracticalExam.java

---
+++ JenkinsPracticalExam.java
@@ -0,0 +1,10 @@
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Jenkins Practical Exam' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Jenkins Practical Exam"); // prints Jenkins Practical Exam 
+   }
+}
